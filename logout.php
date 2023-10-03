9f57c010023bc9ce7e641409b4d39ed62f1d50f5
Deshik <deshiksingamsetty@gmail.com>
2023-10-03 23:06:25 +0530

    Added Logout page

---
+++ logout.php
@@ -0,0 +1,21 @@
+<?php
+// Start the session to access session variables
+session_start();
+
+// Check if the user is logged in (authenticated)
+if (isset($_SESSION['user_id'])) {
+    // Unset all of the session variables
+    session_unset();
+
+    // Destroy the session
+    session_destroy();
+
+    // Redirect to the login page or main page after logout
+    header('Location: index.php'); // Change 'login.php' to the appropriate login page URL
+    exit();
+} else {
+    // If the user is not logged in, simply redirect to the main page
+    header('Location: index.php'); // Change 'main.php' to the appropriate main page URL
+    exit();
+}
+?>
